b8dd1f8a6eecfe3f0569fca3b37c67d7b0b42614
Ajax <15604334993@163.com>
2019-03-26 16:26:55 +0800

    style

--- mvvmsmile/src/main/java/com/kelan/mvvmsmile/widget/SmileItemViewsAdapter.java
+++ mvvmsmile/src/main/java/com/kelan/mvvmsmile/widget/SmileItemViewsAdapter.java
@@ -33,7 +33,7 @@ import androidx.recyclerview.widget.RecyclerView;
  * 一个带 cache 功能的“列表型数据-View”的适配器，适用于自定义 {@link View} 需要显示重复单元 {@link android.widget.ListView} 的情景，
  * cache 功能主要是保证在需要多次刷新数据或布局的情况下（{@link android.widget.ListView} 或 {@link RecyclerView} 的 itemView）
  * 复用已存在的 {@link View}。
- * QMUI 用于 {@link SmileTabSegment 中 {@link SmileTab } 与数据的适配。
+ * SmileUI 用于 {@link SmileTabSegment 中 {@link SmileTab } 与数据的适配。
  *
  * @author cginechen
  * @date 2016-11-27
